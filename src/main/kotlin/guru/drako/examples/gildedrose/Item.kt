ef39ee4baf15d96bcab093f19244bc5f17109688
Felix Bytow <felix.bytow@googlemail.com>
2020-04-10 12:11:31 +0000

    :art: Item is no longer data class

--- src/main/kotlin/guru/drako/examples/gildedrose/Item.kt
+++ src/main/kotlin/guru/drako/examples/gildedrose/Item.kt
@@ -1,7 +1,9 @@
 package guru.drako.examples.gildedrose
 
-data class Item(
+class Item(
   val name: String,
   var sellIn: Int,
   var quality: Int
-)
+) {
+  override fun toString() = """{ name: "$name", sellIn: $sellIn, quality: $quality }"""
+}
