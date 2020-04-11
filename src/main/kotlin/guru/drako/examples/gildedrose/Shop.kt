229d2994b2f702e7a23b5d4617909679a8a24f00
Felix Bytow <drako@drako.guru>
2020-04-11 02:22:48 +0200

    :recycle: extracted sellIn update logic

--- src/main/kotlin/guru/drako/examples/gildedrose/Shop.kt
+++ src/main/kotlin/guru/drako/examples/gildedrose/Shop.kt
@@ -1,6 +1,18 @@
 package guru.drako.examples.gildedrose
 
 class Shop(val items: List<Item>) {
+  companion object {
+    private val LEGENDARY_ITEM_NAMES = listOf(
+      "Sulfuras, Hand of Ragnaros"
+    )
+  }
+
+  private fun Item.updateSellIn() {
+    if (name !in LEGENDARY_ITEM_NAMES) {
+      --sellIn
+    }
+  }
+
   fun updateQuality() {
     for (item in items) {
       if (item.name != "Aged Brie" && item.name != "Backstage passes to a TAFKAL80ETC concert") {
@@ -29,9 +41,7 @@ class Shop(val items: List<Item>) {
         }
       }
 
-      if (item.name != "Sulfuras, Hand of Ragnaros") {
-        --item.sellIn
-      }
+      item.updateSellIn()
 
       if (item.sellIn < 0) {
         if (item.name != "Aged Brie") {
