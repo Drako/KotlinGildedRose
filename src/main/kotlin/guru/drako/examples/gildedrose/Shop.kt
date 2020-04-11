2170c0c2ca1acdca94caa18344e6a2cb576bac14
Felix Bytow <drako@drako.guru>
2020-04-11 02:44:51 +0200

    :recycle: done refactoring

--- src/main/kotlin/guru/drako/examples/gildedrose/Shop.kt
+++ src/main/kotlin/guru/drako/examples/gildedrose/Shop.kt
@@ -1,65 +1,54 @@
 package guru.drako.examples.gildedrose
 
+import kotlin.math.max
+import kotlin.math.min
+
 class Shop(val items: List<Item>) {
   companion object {
     private val LEGENDARY_ITEM_NAMES = listOf(
       "Sulfuras, Hand of Ragnaros"
     )
-  }
 
-  private fun Item.updateSellIn() {
-    if (name !in LEGENDARY_ITEM_NAMES) {
-      --sellIn
+    private const val MIN_QUALITY = 0
+    private const val MAX_QUALITY = 50
+
+    private val Item.baseQualityModifier: Int
+      get() = when (name) {
+        "Aged Brie", "Backstage passes to a TAFKAL80ETC concert" -> 1
+        in LEGENDARY_ITEM_NAMES -> 0
+        else -> -1
+      }
+
+    private val Item.qualityFactor: Int
+      get() = when (name) {
+        "Backstage passes to a TAFKAL80ETC concert" -> when {
+          sellIn <= 0 -> -MAX_QUALITY
+          sellIn <= 5 -> 3
+          sellIn <= 10 -> 2
+          else -> 1
+        }
+        else -> if (sellIn <= 0) 2 else 1
+      }
+
+    private fun Item.updateQuality(newValue: Int) {
+      if (name !in LEGENDARY_ITEM_NAMES) {
+        quality = min(MAX_QUALITY, max(MIN_QUALITY, newValue))
+      }
+    }
+
+    private fun Item.updateSellIn() {
+      if (name !in LEGENDARY_ITEM_NAMES) {
+        --sellIn
+      }
     }
   }
 
   fun updateQuality() {
     for (item in items) {
-      if (item.name != "Aged Brie" && item.name != "Backstage passes to a TAFKAL80ETC concert") {
-        if (item.quality > 0) {
-          if (item.name != "Sulfuras, Hand of Ragnaros") {
-            --item.quality
-          }
-        }
-      } else {
-        if (item.quality < 50) {
-          ++item.quality
-
-          if (item.name == "Backstage passes to a TAFKAL80ETC concert") {
-            if (item.sellIn < 11) {
-              if (item.quality < 50) {
-                ++item.quality
-              }
-            }
-
-            if (item.sellIn < 6) {
-              if (item.quality < 50) {
-                ++item.quality
-              }
-            }
-          }
-        }
-      }
-
+      val baseModifier = item.baseQualityModifier
+      val factor = item.qualityFactor
+      item.updateQuality(newValue = item.quality + baseModifier * factor)
       item.updateSellIn()
-
-      if (item.sellIn < 0) {
-        if (item.name != "Aged Brie") {
-          if (item.name != "Backstage passes to a TAFKAL80ETC concert") {
-            if (item.quality > 0) {
-              if (item.name != "Sulfuras, Hand of Ragnaros") {
-                --item.quality
-              }
-            }
-          } else {
-            item.quality -= item.quality
-          }
-        } else {
-          if (item.quality < 50) {
-            ++item.quality
-          }
-        }
-      }
     }
   }
 }
