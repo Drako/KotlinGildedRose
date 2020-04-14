d83f6fe8e3063ac2a2ef5a4d2dcb7da4bf291334
Felix Bytow <drako@drako.guru>
2020-04-14 11:59:22 +0200

    :recycle: improved handling of items with increasing quality

--- src/main/kotlin/guru/drako/examples/gildedrose/Shop.kt
+++ src/main/kotlin/guru/drako/examples/gildedrose/Shop.kt
@@ -3,48 +3,39 @@ package guru.drako.examples.gildedrose
 import kotlin.math.max
 import kotlin.math.min
 
+private const val MIN_QUALITY = 0
+private const val MAX_QUALITY = 50
+
+private val LEGENDARY_ITEMS = setOf("Sulfuras, Hand of Ragnaros")
+
+private fun Item.isLegendary(): Boolean = name in LEGENDARY_ITEMS
+
+private fun Item.isBackstagePass(): Boolean = name.startsWith(prefix = "backstage passes", ignoreCase = true)
+
+private fun Item.isAged(): Boolean = name.startsWith(prefix = "aged", ignoreCase = true)
+
+private fun Item.hasIncreasingValue(): Boolean = isBackstagePass() || isAged()
+
+private fun Item.isConjured(): Boolean = name.startsWith(prefix = "conjured", ignoreCase = true)
+
+private fun Item.baseQualityModifier(): Int = if (hasIncreasingValue()) 1 else -1
+
+private fun Item.qualityFactor(): Int = when {
+  isBackstagePass() -> when {
+    sellIn <= 0 -> -MAX_QUALITY
+    sellIn <= 5 -> 3
+    sellIn <= 10 -> 2
+    else -> 1
+  }
+  isConjured() -> if (sellIn <= 0) 4 else 2
+  else -> if (sellIn <= 0) 2 else 1
+}
+
 class Shop(val items: List<Item>) {
-  companion object {
-    private val LEGENDARY_ITEMS = setOf(
-      "Sulfuras, Hand of Ragnaros"
-    )
-
-    private val ITEMS_WITH_INCREASING_VALUE = setOf(
-      "Aged Brie",
-      "Backstage passes to a TAFKAL80ETC concert"
-    )
-
-    private const val MIN_QUALITY = 0
-    private const val MAX_QUALITY = 50
-
-    private val Item.baseQualityModifier: Int
-      get() = when (name) {
-        in ITEMS_WITH_INCREASING_VALUE -> 1
-        else -> -1
-      }
-
-    private val Item.qualityFactor: Int
-      get() = when {
-        name.startsWith(prefix = "backstage passes", ignoreCase = true) -> when {
-          sellIn <= 0 -> -MAX_QUALITY
-          sellIn <= 5 -> 3
-          sellIn <= 10 -> 2
-          else -> 1
-        }
-        name.startsWith(prefix = "conjured", ignoreCase = true) -> if (sellIn <= 0) 4 else 2
-        else -> if (sellIn <= 0) 2 else 1
-      }
-  }
-
-  fun updateQuality() {
-    items
-      .asSequence()
-      .filter { it.name !in LEGENDARY_ITEMS }
-      .forEach {
-        with(it) {
-          quality = min(MAX_QUALITY, max(MIN_QUALITY, quality + baseQualityModifier * qualityFactor))
-          --sellIn
-        }
-      }
-  }
+  fun updateQuality() = items.asSequence()
+    .filterNot(Item::isLegendary)
+    .forEach {
+      it.quality = min(MAX_QUALITY, max(MIN_QUALITY, with(it) { quality + baseQualityModifier() * qualityFactor() }))
+      --it.sellIn
+    }
 }
