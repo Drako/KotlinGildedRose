d4dbef686000d91edc68174cd774054490a6c810
Felix Bytow <drako@drako.guru>
2020-07-24 23:01:20 +0200

    :recycle::fire: simplified implementation

--- src/main/kotlin/guru/drako/examples/gildedrose/Shop.kt
+++ src/main/kotlin/guru/drako/examples/gildedrose/Shop.kt
@@ -11,30 +11,27 @@ private fun Item.isBackstagePass(): Boolean = name.startsWith(prefix = "backstag
 
 private fun Item.isAged(): Boolean = name.startsWith(prefix = "aged", ignoreCase = true)
 
-private fun Item.hasIncreasingValue(): Boolean = isBackstagePass() || isAged()
-
 private fun Item.isConjured(): Boolean = name.startsWith(prefix = "conjured", ignoreCase = true)
 
 private inline fun <T> Sequence<T>.forEachWith(action: T.() -> Unit) = forEach { it.action() }
 
-private fun Item.baseQualityModifier(): Int = if (hasIncreasingValue()) 1 else -1
-
-private fun Item.qualityFactor(): Int = when {
+private fun Item.qualityModifier(): Int = when {
   isBackstagePass() -> when {
     sellIn <= 0 -> -MAX_QUALITY
     sellIn <= 5 -> 3
     sellIn <= 10 -> 2
     else -> 1
   }
-  isConjured() -> if (sellIn <= 0) 4 else 2
-  else -> if (sellIn <= 0) 2 else 1
+  isAged() -> if (sellIn <= 0) 2 else 1
+  isConjured() -> if (sellIn <= 0) -4 else -2
+  else -> if (sellIn <= 0) -2 else -1
 }
 
 class Shop(val items: List<Item>) {
   fun updateQuality() = items.asSequence()
     .filterNot(Item::isLegendary)
     .forEachWith {
-      quality = (quality + baseQualityModifier() * qualityFactor()).coerceIn(MIN_QUALITY, MAX_QUALITY)
+      quality = (quality + qualityModifier()).coerceIn(MIN_QUALITY, MAX_QUALITY)
       --sellIn
     }
 }
